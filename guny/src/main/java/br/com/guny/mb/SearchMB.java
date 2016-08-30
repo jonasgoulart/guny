d801a3e1ae84ac7a3e458bd2e66a999116b7a463
jonas.goulart <jonas.goulart@disop02390>
2016-08-30 18:54:39 -0300

    Configuração da estrutura do projeto

--- guny/src/main/java/br/com/guny/mb/SearchMB.java
+++ guny/src/main/java/br/com/guny/mb/SearchMB.java
@@ -1,12 +1,28 @@
 package br.com.guny.mb;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
+@RequestScoped
 @ManagedBean(name="searchMB")
-public class SearchMB {
+public class SearchMB implements Serializable{
 
-	public void search(){
-		
-		System.out.println("Search");
+	private static final long serialVersionUID = 1L;
+	
+	private String searchValue;
+
+	public String search(){		
+		System.out.println("Search for "+searchValue);
+		return null;
+	}
+
+	public String getSearchValue() {		
+		return searchValue;
+	}
+
+	public void setSearchValue(String searchValue) {		
+		this.searchValue = searchValue;
 	}
 }
