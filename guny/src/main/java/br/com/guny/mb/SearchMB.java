d333c12b0aafaca3658dd64739243cea76cc1d03
jonas.goulart <jonas.goulart@disop02390>
2016-08-30 17:03:08 -0300

    Projeto Guny

---
+++ guny/src/main/java/br/com/guny/mb/SearchMB.java
@@ -0,0 +1,12 @@
+package br.com.guny.mb;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="searchMB")
+public class SearchMB {
+
+	public void search(){
+		
+		System.out.println("Search");
+	}
+}
